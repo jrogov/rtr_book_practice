9582dc3641a5d40e3d2d5135cdca8667d80c2cf5
RogovYaroslav <rogovyaroslav@gmail.com>
2017-04-11 18:11:39 +0300

    Add plane generation and set_params to texture.c

--- src/io/texture.c
+++ src/io/texture.c
@@ -10,8 +10,54 @@
 #include "bmp.h"
 #include "texture.h"
 
-GLuint load_texture( image_t* image
-	){
+GLuint gen_plane_tex(GLint format, GLsizei width, GLsizei height, GLenum type, texparam_t* params)
+{
+	GLuint target;
+	glGenTextures(1, &target);
+	glBindTexture(GL_TEXTURE_2D, target);
+	glTexImage2D( GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_FLOAT, NULL);
+	set_tex_params(GL_TEXTURE_2D, params);
+	return target;
+}
+
+void set_tex_params(GLuint target, texparam_t* params)
+{
+	for( texparam_t* p = &params[0] ; p->type != 0 ; ++p )
+	{
+		switch(p->type)
+		{
+			case GL_DEPTH_STENCIL_TEXTURE_MODE:
+			case GL_TEXTURE_BASE_LEVEL:
+			case GL_TEXTURE_COMPARE_FUNC:
+			case GL_TEXTURE_COMPARE_MODE:
+			case GL_TEXTURE_MIN_FILTER:
+			case GL_TEXTURE_MAG_FILTER:
+			case GL_TEXTURE_MAX_LEVEL:
+			case GL_TEXTURE_SWIZZLE_R:
+			case GL_TEXTURE_SWIZZLE_G:
+			case GL_TEXTURE_SWIZZLE_B:
+			case GL_TEXTURE_SWIZZLE_A:
+			case GL_TEXTURE_WRAP_S:
+			case GL_TEXTURE_WRAP_T:
+			case GL_TEXTURE_WRAP_R:
+				glTexParameteri(target, p->type, p->value.i);
+				printf("ERROR CODE = %d\n", glGetError());
+				printf("0x%X 0x%X\n", p->type, p->value.i);
+				break;
+
+			case GL_TEXTURE_LOD_BIAS:
+			case GL_TEXTURE_MIN_LOD:
+			case GL_TEXTURE_MAX_LOD:
+				glTexParameterf(target, p->type, p->value.f);
+				printf("ERROR CODE = %d\n", glGetError());
+				printf("0x%X %.0f\n", p->type, p->value.f);
+				break;
+		}
+	}
+}
+
+GLuint load_texture( image_t* image, texparam_t* params )
+{
 	GLuint textureID;
 	glGenTextures(1, &textureID);
 
@@ -24,10 +70,14 @@ GLuint load_texture( image_t* image
 		GL_UNSIGNED_BYTE,
 		image->pixels);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	if( params != NULL )
+		set_tex_params(GL_TEXTURE_2D, params);
+
+
+	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
 /*
 	glEnable(GL_BLEND);
@@ -37,28 +87,28 @@ GLuint load_texture( image_t* image
 	return textureID;
 }
 
-GLuint fload_BMP_texture( const char* filename ){
-	FILE* 	f;
+IO_stat_t fload_BMP_texture( const char* filename, texparam_t* params, GLuint *textureID )
+{
+	FILE* f;
 	image_t* image;
 	IO_stat_t status;
-	GLuint textureID;
 
 	f = fopen(filename, "r");
 	if( NULL == f ) {
 		wflog("ERROR", "%s not found", filename);
-		return 0;
+		return get_last_iostat();
 	}
 
 	status = load_BMP24(f, &image, NULL);
 	if ( IO_OK != status ){
 		wflog("ERROR", "BMP loading error (io code %u)", status);
-		return 0;
+		return status;
 	}
 
 	wflog("INFO", "Loaded texture %s (%dx%d)", filename, image->width, image->height);
 
-	textureID = load_texture(image);
+	*textureID = load_texture(image, params);
 
 	free_image(image);
-	return textureID;
+	return IO_OK;
 }
--- src/io/texture.h
+++ src/io/texture.h
@@ -2,8 +2,29 @@
 #define _TEXTURES_H_
 
 #include "image.h"
+#include <stdint.h>
 
-GLuint load_texture( image_t* image );
-GLuint fload_BMP_texture( const char* filename );
+
+typedef struct
+{
+	GLenum type;
+	union
+	{
+		GLint i;
+		GLfloat f;
+	} value;
+}
+texparam_t;
+
+
+GLuint gen_plane_tex(GLint format, GLsizei width, GLsizei height, GLenum type, texparam_t* params);
+GLuint load_texture( image_t* image, texparam_t* params );
+IO_stat_t fload_BMP_texture( const char* filename, texparam_t* params, GLuint *textureID );
+void set_tex_params( GLuint target, texparam_t* params );
+
+
+#define TEXPARAM(a, b) { (a), (b) }
+#define TEXPARAM_END { 0, 0 }
 
 #endif /* _TEXTURES_H_ */
+
